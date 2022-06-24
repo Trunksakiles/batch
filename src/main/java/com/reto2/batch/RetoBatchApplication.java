006f580c5dee9eac0f159753cdaaeedb0af30938
Trunksakiles <alexito.ge@gmail.com>
2022-06-24 09:49:49 -0500

    batch

---
+++ src/main/java/com/reto2/batch/RetoBatchApplication.java
@@ -0,0 +1,13 @@
+package com.reto2.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reto2/batch/RetoBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reto2.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
